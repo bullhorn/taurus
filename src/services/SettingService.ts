ba143936784c5d86caf159267623a3a6915dc877
Joshua Godi <joshuagodi@gmail.com>
2018-05-15 14:02:48 -0500

    feat(settings): Adding a settings/entitlement service (#3)
    * feat(settings): Adding a settings/entitlement service
    
    * feat(settings): Adding a settings/entitlement service
    
    * feat(settings): Adding a settings/entitlement service

---
+++ src/services/SettingService.ts
@@ -0,0 +1,33 @@
+import { AxiosInstance, AxiosResponse } from 'axios';
+import { BullhornAllSettingsAndEntitlementsResponse } from '../types';
+import { Staffing } from './Staffing';
+
+/**
+ * A Class that defines a service to grab settings from Bullhorn
+ */
+export class SettingService {
+  http: AxiosInstance;
+
+  constructor() {
+    this.http = Staffing.http();
+  }
+
+  async getSettings(settings: string[]): Promise<{ [key: string]: any }> {
+    const response: AxiosResponse = await this.http.get(`settings/${settings.join()}`);
+    return response.data;
+  }
+
+  async getEntitlements(entity: string): Promise<string[]> {
+    const response: AxiosResponse = await this.http.get(`entitlements/${entity}`);
+    return response.data;
+  }
+
+  async getAllSettingsAndEntitlements(): Promise<BullhornAllSettingsAndEntitlementsResponse> {
+    const response: AxiosResponse = await this.http.get('services/Settings/allEntitlementsAndSettings');
+    const result: BullhornAllSettingsAndEntitlementsResponse = response.data;
+    if (result && result.dashboardEntitlements && result.entitlements) {
+      result.entitlements.DASHBOARD = result.dashboardEntitlements;
+    }
+    return result;
+  }
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,7 @@ export * from './EntityService';
 export * from './MetaService';
 export * from './QueryService';
 export * from './SearchService';
+export * from './SettingService';
 export * from './Staffing';
 export * from './StaffingAuthProvider';
 export * from './Where';
