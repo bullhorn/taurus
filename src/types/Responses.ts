50e8f3eef7c75b11c0292b5f2c615cd8450bda4e
Joshua Godi <joshuagodi@gmail.com>
2018-05-15 11:27:31 -0500

    feat(settings): Adding a settings/entitlement service

--- src/types/Responses.ts
+++ src/types/Responses.ts
@@ -1,44 +1,79 @@
 import { Field } from './Field';
 
+export interface BullhornAllSettingsAndEntitlementsResponse {
+  settings: { [key: string]: any };
+  dashboardEntitlements: string[];
+  entitlements: { [key: string]: string[] };
+  userData: {
+    id: number;
+    masterUserID: number;
+    userType: {
+      id: number;
+      name: string;
+    };
+    firstName: string;
+    lastName: string;
+    phone: string;
+    mobile: string;
+    useDefaultMailClient: boolean;
+    email: string;
+    address: {
+      address1: string;
+      address2: string;
+      city: string;
+      state: string;
+      zip: string;
+      countryID: string;
+      countryName: string;
+      countryCode: string;
+    };
+    departments: {
+      total: number;
+      data: { id: number; name: string }[];
+    };
+    srelease: boolean;
+  };
+}
+
 export interface BullhornMessage {
-    detailMessage: string;
-    severity: string;
-    type: string;
+  detailMessage: string;
+  severity: string;
+  type: string;
 }
 
 export interface BullhornErrorMessage {
-    errorMessage: string;
-    errorMessageKey: string;
-    errorCode: number;
+  errorMessage: string;
+  errorMessageKey: string;
+  errorCode: number;
 }
 
 export interface BullhornMetaResponse {
-    entity: string;
-    entityMetaUrl: string;
-    label: string;
-    dateLastModified: number;
-    fields: Field[];
+  entity: string;
+  entityMetaUrl: string;
+  label: string;
+  dateLastModified: number;
+  fields: Field[];
 }
 
 export interface BullhornListResponse<T> {
-    total?: number;
-    start: string;
-    count: string;
-    data: T[];
-    messages?: BullhornMessage[];
-    meta?: BullhornMetaResponse;
+  total?: number;
+  start: string;
+  count: string;
+  data: T[];
+  messages?: BullhornMessage[];
+  meta?: BullhornMetaResponse;
 }
 
 export interface BullhornEntityResponse<T> {
-    data: T[];
-    messages?: BullhornMessage[];
-    meta?: BullhornMetaResponse;
+  data: T[];
+  messages?: BullhornMessage[];
+  meta?: BullhornMetaResponse;
 }
 
 export interface BullhornSavedEntityResponse<T> {
-    changedEntityType: string;
-    changedEntityId: number;
-    changeType: 'INSERT' | 'UPDATE';
-    messages: string[];
-    data: T;
+  changedEntityType: string;
+  changedEntityId: number;
+  changeType: 'INSERT' | 'UPDATE';
+  messages: string[];
+  data: T;
 }
