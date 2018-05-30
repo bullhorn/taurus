3a57681bcb5e6f2fbdd5a30ecc00838e3e642e72
Joshua Godi <joshuagodi@gmail.com>
2018-05-30 09:05:32 -0500

    fix(SetingsService): Put Dashboard entitlements on the correct object

--- src/services/SettingService.ts
+++ src/services/SettingService.ts
@@ -6,28 +6,28 @@ import { Staffing } from './Staffing';
  * A Class that defines a service to grab settings from Bullhorn
  */
 export class SettingService {
-  http: AxiosInstance;
+    http: AxiosInstance;
 
-  constructor() {
-    this.http = Staffing.http();
-  }
-
-  async getSettings(settings: string[]): Promise<{ [key: string]: any }> {
-    const response: AxiosResponse = await this.http.get(`settings/${settings.join()}`);
-    return response.data;
-  }
-
-  async getEntitlements(entity: string): Promise<string[]> {
-    const response: AxiosResponse = await this.http.get(`entitlements/${entity}`);
-    return response.data;
-  }
-
-  async getAllSettingsAndEntitlements(): Promise<BullhornAllSettingsAndEntitlementsResponse> {
-    const response: AxiosResponse = await this.http.get('services/Settings/allEntitlementsAndSettings');
-    const result: BullhornAllSettingsAndEntitlementsResponse = response.data;
-    if (result && result.dashboardEntitlements && result.entitlements) {
-      result.entitlements.DASHBOARD = result.dashboardEntitlements;
+    constructor() {
+        this.http = Staffing.http();
     }
-    return result;
-  }
+
+    async getSettings(settings: string[]): Promise<{ [key: string]: any }> {
+        const response: AxiosResponse = await this.http.get(`settings/${settings.join()}`);
+        return response.data;
+    }
+
+    async getEntitlements(entity: string): Promise<string[]> {
+        const response: AxiosResponse = await this.http.get(`entitlements/${entity}`);
+        return response.data;
+    }
+
+    async getAllSettingsAndEntitlements(): Promise<BullhornAllSettingsAndEntitlementsResponse> {
+        const response: AxiosResponse = await this.http.get('services/Settings/allEntitlementsAndSettings');
+        const result: BullhornAllSettingsAndEntitlementsResponse = response.data;
+        if (result && result.dashboardEntitlements && result.entitlements) {
+            result.entitlements.Dashboard = result.dashboardEntitlements;
+        }
+        return result;
+    }
 }
