1a40c72fc0f192b39e8e57f45ae05b69f66b50ff
Elizabeth Carretto <escarretto@gmail.com>
2018-06-06 12:18:10 -0500

    fix(EntityService): Implementing edit/many

--- src/core/Entity.ts
+++ src/core/Entity.ts
@@ -169,34 +169,25 @@ export class Entity<T extends Identity> extends StatefulSubject<T> {
         });
         return this;
     }
-    // /**
-    // * Make http request to get entity. Objects 'data' property will be set to response, then promise will be resolved.
-    // * @param property - The TO_MANY Association field
-    // * @param fields - Additional fields to retrieve on the TO_MANY field
-    // */
-    // Many(property: string, fields: Array<string>): Promise<AxiosResponse> {
-    //     Return this.http.get(`${this.endpoint}${this.value.id}/${property}`, {
-    //         Params: {
-    //             Fields: fields,
-    //             ShowTotalMatched: true
-    //         }
-    //     }).then((response: AxiosResponse) => {
-    //         If (!this.value.hasOwnProperty(property)) {
-    //             Object.defineProperty(this, property, {
-    //                 Get: function getter() {
-    //                     Return this.value[property];
-    //                 },
-    //                 Set: function setter(value) {
-    //                     This.value[property] = value;
-    //                 },
-    //                 Configurable: true,
-    //                 Enumerable: true
-    //             });
-    //         }
-    //         This.value[property] = response.data;
-    //         Return response;
-    //     });
-    // }
+    /**
+     * Make http request to get entity's many relationship. Response will update the value of the entity object.
+     * @param property - The TO_MANY Association field
+     * @param fields - Additional fields to retrieve on the TO_MANY field
+     */
+    many(property: string, fields: string[]): Entity<T> {
+        // tslint:disable-next-line:no-floating-promises
+        this.$entity.many(property, fields, this.value).then((response: AxiosResponse) => {
+            if (!this._fields.includes(property)) {
+                this._proxy(property);
+                this._fields.push(property);
+            }
+            this.value[property] = response;
+            this.patch({ property: response });
+            this._setUpObservable(this.getValue());
+            return response;
+        });
+        return this;
+    }
 
     /**
      * Create or Updates the entity based on the presence of an 'id' property
