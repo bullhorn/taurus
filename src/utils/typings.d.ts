e20b7db056825f474adaad5834d331923e8ecf3b
Amrutha Rajiv <arajiv@bullhorn.com>
2019-05-22 11:01:59 -0400

    fix(cache) enforcing browser storage limits

---
+++ src/utils/typings.d.ts
@@ -0,0 +1,16 @@
+interface Window {
+  opr?: any;
+  InstallTrigger?: any;
+  HTMLElement?: any;
+  StyleMedia?: any;
+  chrome?: any;
+  CSS?: any;
+  opera?: any;
+}
+interface Document {
+  documentMode?: any;
+}
+declare var opr: any;
+declare var InstallTrigger: any;
+declare var safari: any;
+
