53f73e6c37aff74cd5456ceaa33377c6992ad137
Dan Voegelin <dvoegelin@bullhorn.com>
2022-06-28 16:54:32 -0400

    fixing bug with blank query

--- src/services/QueryService.ts
+++ src/services/QueryService.ts
@@ -77,7 +77,9 @@ export class QueryService<T> {
     return this;
   }
   appendQuery(value: string) {
-    this.parameters.where = this.parameters.where?.length ? `${this.parameters.where} AND ${value}` : value;
+    if (value.length) {
+      this.parameters.where = this.parameters.where?.length ? `${this.parameters.where} AND ${value}` : value;
+    }
     return this;
   }
   count(value: number) {
