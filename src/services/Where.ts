2630d3ebcb6d653cd5c6fa39329d5cef21524184
Michael Dill <michael.dill@bullhorn.com>
2023-06-30 14:30:57 -0500

    update to prefix with $

--- src/services/Where.ts
+++ src/services/Where.ts
@@ -129,7 +129,7 @@ export class Where {
           queries.push(`(${Where.parseQueryValue(subkey, value[subkey])})`);
         }
         return `(${queries.join(' OR ')})`;
-      } else if (key.includes('group_') || key === 'group') {
+      } else if (key.includes('$group_') || key === '$group') {
         queries.push(`(${Where.toQuerySyntax(value)})`);
       } else {
         queries.push(Where.parseQueryValue(key, value));
--- test/Where.spec.ts
+++ test/Where.spec.ts
@@ -330,13 +330,13 @@ describe('Where', () => {
     it('should create a valid lucene query for A AND((B OR C) AND (D OR E))', () => {
       const where = Where.toSearchSyntax({
         id: 103,
-        group_1: {
+        $group_1: {
           or: {
             firstName: 'test',
             lastName: 'test'
           }
         },
-        group_2: {
+        $group_2: {
           or: {
             owner: 'me',
             title: 'me'
@@ -348,13 +348,13 @@ describe('Where', () => {
     it('should create a valid lucene query with groups for A AND (B OR C) AND (D.id OR E.id)', () => {
       const where = Where.toSearchSyntax({
         id: 103,
-        group_1: {
+        $group_1: {
           or: {
             firstName: 'test',
             lastName: 'test'
           }
         },
-        group_2: {
+        $group_2: {
           or: {
             owner: {
               id: 103
@@ -369,14 +369,14 @@ describe('Where', () => {
     });
     it('should create a valid lucene query with groups for (A AND (B OR C)) AND (D AND (E OR F))', () => {
       const where = Where.toSearchSyntax({
-        group_1: {
+        $group_1: {
           id: 103,
           or: {
             firstName: 'test',
             lastName: 'test'
           }
         },
-        group_2: {
+        $group_2: {
           id: 103,
           or: {
             owner: {
